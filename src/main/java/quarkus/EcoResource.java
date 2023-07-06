50714860f93311cf07b7f513afe28373ea7859e3
Alvaro Martinez <alvaro.martinez.lopez-abad@alumnos.upm.es>
2023-07-06 14:05:16 +0200

    aA±adido @QueryParam y @PathParam para gestionar parametros por UIRL

--- src/main/java/quarkus/EcoResource.java
+++ src/main/java/quarkus/EcoResource.java
@@ -2,11 +2,27 @@ package quarkus;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.QueryParam;
+
+import java.util.Optional;
 
 @Path("/saludar")
 public class EcoResource {
     @GET
-    public String saludar(){
-        return "Hola muy buenas";
+    public String saludar(@QueryParam("mensaje") String mensaje){
+        return Optional.ofNullable(mensaje).map(n -> "> " + n).orElse("No se muy bien que decirte");
+    }
+
+    @GET
+    @Path("/{nombre}") //Los parametros entre llaves son parametros de la URL, pero no de query (query -> {URL}?param=valor)
+    public String saludo(@PathParam("nombre") String nombre){
+        return "Hola " + nombre;
+    }
+
+    @GET
+    @Path("/{nombre}/mayusculas")
+    public String gritar(@PathParam("nombre") String nombre){
+        return "HOLA " + nombre.toUpperCase();
     }
 }
